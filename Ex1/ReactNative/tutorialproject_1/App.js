a882feabd4563c9705c60cffd03d0d3210bca15b
Hieu Nguyen <hieultt0905@gmail.com>
2019-01-13 22:42:27 +0700

    thu lan 2

--- Ex1/ReactNative/tutorialproject_1/App.js
+++ Ex1/ReactNative/tutorialproject_1/App.js
@@ -2,20 +2,30 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
+    render() {
+        return (
+            <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
-    );
-  }
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const styles1 = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+
+const styles2 = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
